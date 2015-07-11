24395bd60166121f16c17fb263ef34b7ee2708f4
Sergio Moreno <sergio_mf18@hotmail.com>
2015-07-11 19:42:13 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,14 @@
+//GET /quizes/question
+exports.question = function(req,res){
+	res.render('quizes/question', {pregunta : 'Capital de España'});
+}
+
+//GET /quizes/answer
+exports.answer = function(req,res){
+	if(req.query.respuesta === "Madrid"){
+		res.render('quizes/answer', {respuesta : 'Correcta'});
+	} else {
+		res.render('quizes/answer', {respuesta : 'Incorrecta'});		
+	}
+
+}
